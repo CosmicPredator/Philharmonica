46b56bd12892e01bccf3d9bf6f669bc665142efe
CosmicPredator <baranikumars2003@gmail.com>
2026-07-30 10:14:51 +0530

    code: Create project scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use slint::ComponentHandle;
+
+mod ui {
+    slint::include_modules!();
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let window = ui::AppWindow::new()?;
+
+    let window_handle = window.as_weak();
+    window.on_request_increase_value(move || {
+        if let Some(window_handle) = window_handle.upgrade() {
+            window_handle.set_counter(window_handle.get_counter() + 1);
+        }
+    });
+
+    window.run()?;
+
+    Ok(())
 }
