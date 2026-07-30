1883cce0f6ce9a34f55fd66297743754d14b809f
CosmicPredator <baranikumars2003@gmail.com>
2026-07-30 11:01:44 +0530

    Added sidebar component and root view added: Sidebar component added: RootView componenet added: SVG Icons for Sidebar component menus
    Signed-off-by: CosmicPredator <baranikumars2003@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,6 @@ mod ui {
 fn main() -> Result<(), Box<dyn Error>> {
     let window = ui::AppWindow::new()?;
 
-    let window_handle = window.as_weak();
-    window.on_request_increase_value(move || {
-        if let Some(window_handle) = window_handle.upgrade() {
-            window_handle.set_counter(window_handle.get_counter() + 1);
-        }
-    });
-
     window.run()?;
 
     Ok(())
